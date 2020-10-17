49e356f634f2a7257a9619a6c449c16ec4d08c3d
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-10-18 01:37:53 +0530

    tutorial8 updated

--- tutorial8.py
+++ tutorial8.py
@@ -9,14 +9,15 @@ val = "jayant"
 print(val.capitalize()) # To convert the first letter to capitals
 
 #-----------------------------------------------------------------------------------------------------------------------
-# casefold
+# casefold() and lower()
 val1 = "JAYANT"
-print(val1.casefold()) # To convert string to lowercase
+print("casefold : ",val1.casefold()) # To convert string to lowercase
+print("lower    : ",val1.lower())
 
 # ----------------------------------------------------------------------------------------------------------------------
 # center()
 val2 = "jayant"
-print(val2.center(50)) # Prints the string in the centre of specified number of blank spaces
+print("center : ",val2.center(50),"end") # Prints the string in the centre of specified number of blank spaces
 
 #-----------------------------------------------------------------------------------------------------------------------
 # count()
@@ -74,16 +75,16 @@ Description : The index() method is almost the same as the find() method, the on
 """
 ival6 = "My name is Slim Shady"
 print("index :",ival6.index("Slim")) # --> Returns Index 11 as 'S' of Slim starts at that location
-print("index :",ival6.index("xyz"))  # --> This will return an exception
+# print("index :",ival6.index("xyz"))  # --> This will return an exception (Commented out as it will stop the code execution. Uncomment to test)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 """
 format()
-Syntax : The format() method formats the specified value(s) and insert them inside the string's placeholder.
-         The placeholder is defined using curly brackets: {}.
+Description : The format() method formats the specified value(s) and insert them inside the string's placeholder.
+              The placeholder is defined using curly brackets: {}.
          
-         txt1 = "My name is {fname}, I'am {age}".format(fname = "John", age = 36) --> by name
+Syntax : txt1 = "My name is {fname}, I'am {age}".format(fname = "John", age = 36) --> by name
          txt2 = "My name is {0}, I'am {1}".format("John",36)  --> by index
          txt3 = "My name is {}, I'am {}".format("John",36)  --> by position
 """
@@ -106,4 +107,32 @@ isprintable()	Returns True if all characters in the string are printable
 isspace()	    Returns True if all characters in the string are whitespaces
 istitle()	    Returns True if the string follows the rules of a title
 isupper()	    Returns True if all characters in the string are upper case
+"""
+#-----------------------------------------------------------------------------------------------------------------------
+"""
+join()
+Description : Joins all items in a tuple into a string  
+Syntax : string.join(tuple)
+"""
+arr = ("This","is","one","sentence")
+str1 = "XYX"
+
+x = str1.join(arr)
+print("join :",x)
+print("join : ","".join(arr))
+
+#-----------------------------------------------------------------------------------------------------------------------
+"""
+ljust()
+Description : appends the character specified number of specified times after the string. Default character is a whitespace
+Syntax : string.1just(length,character)  
+"""
+val8 = "Jayant"
+print("ljust : ",val8.ljust(20),"rocks")
+print("ljust : ",val8.ljust(15,"x")) # NOTE : The character can only be one character long. Else it will return an exception.
+
+#------------------------------------------------------------------------------------------------------------------------
+"""
+lstrip() and rstrip()
+Description : 
 """
