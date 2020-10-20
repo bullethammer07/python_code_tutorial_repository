668136a089e8e6902e73379701dea8a43cd79db0
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-10-20 14:17:26 +0530

    tut9 update

--- tutorial9.py
+++ tutorial9.py
@@ -10,6 +10,11 @@
 # bool()             : Returns the boolean value of a specified object (Any object, like String, List, Number etc).
 # bytearray()        : Both bytearray() and bytes() returns a bytearray object.
 # bytes()            : Both bytearray() and bytes() returns a bytearray object.
+# callable()         : Returns True if the specified object is callable, otherwise it returns False.
+# chr()              : Returns the character that represents the specified unicode.
+# ord()              : Returns the number representing the unicode code of a specified character.
+# compile()          : Returns the specified source as a code object, ready to be executed.
+# eval()             : Evaluates the specified expression, if the expression is a legal Python statement, it will be executed.
 
 #----------------------------------------------------------------------------------------------------------------------
 # abs()
@@ -90,3 +95,70 @@ print("bytearray : ",bytearray("xyz","big5")) # specify the encoding in double q
 print("\n")
 print("bytes : ",bytes(6))
 print("bytes : ",bytes("xyz","big5"))
+
+#----------------------------------------------------------------------------------------------------------------------
+# callable()
+# Description : Returns True if the specified object is callable, otherwise it returns False.
+# Syntax : callable(object)
+#          NOTE : A normal variable is not callable.
+
+print("\n")
+def func():
+  print("Function call")
+
+print("callable : ",callable(func)) # Returns true
+
+var1 = 5;
+print("callable : ",callable(var1))
+
+#----------------------------------------------------------------------------------------------------------------------
+# chr() and ord()
+# Description : chr() Returns the character that represents the specified unicode.
+#               ord() Returns the number representing the unicode code of a specified character.
+# Syntax : chr(number)
+#          number : An integer representing a valid Unicode code point
+#          ord(character)
+#          character : string, any character
+
+print("\n")
+print("chr : ",chr(97),chr(120),chr(36))
+print("ord : ",ord("a"),ord("x"),ord("$"))
+
+#----------------------------------------------------------------------------------------------------------------------
+# eval()
+# Description : eval() : Evaluates the specified expression, if the expression is a legal Python statement, it will be executed.
+#                        In simple words, the eval function evaluates the “String” like a python expression and returns the result as an integer.
+#                        The return value would be the result of the evaluated expression. Often the return type would be an integer.
+#                        Q : Where is the eval function mostly used?
+#                        Ans : Eval function is mostly used in situations or applications which need to evaluate mathematical expressions.
+#                              Also if the user wants to evaluate the string into code then can use eval function, because eval function evaluates the string expression and returns the integer as a result.
+#                        Q : What is the difference between the input() and eval()?
+#                        Ans : the input() takes the user input, but when the user enters an integer as an input the input function returns a string,
+#                              but in the case of eval it will evaluate the returned value from a string to an integer.
+# Syntax : eval(expression,globals,locals)
+#            expresion : A string, that will be evaluates as Python code.
+#            globals : (Optional). A dictionary containing global variables.
+#            locals : (Optional). A dictionary containing local variables.
+
+print("\n")
+
+input_int = input("Enter Integer :: ")
+print(input_int,type(input_int)) # the type will be returned as 'str' i.e string type
+                                 # NOTE : If you enter an expression such as '10+10' it will not be evaluated as an interger with output 20.
+                                 #        instead it will be returned as a string -> '10+10'
+
+input_int2 = eval(input("Enter Integer :: "))
+print(input_int2,type(input_int2)) # the type will be returned as 'int' i.e integer type
+                                   # NOTE : You can also enter an expression here like '10+20' and the output will be evaluated as 30 and as integer
+
+# x = 'print(\"Hello World\")  print(\"This is text 1\")  print(\"this is text 2\")'
+x = 'print("Hello world") | print("Jayant")' # multi print
+y = 'print("xyx")' # single print
+z = 'val1 = 10  | print(val1)'
+# eval(y)
+# eval(z)
+# eval(x)
+
+#----------------------------------------------------------------------------------------------------------------------
+# compile()
+# Description :  returns the specified source as a code object, ready to be executed.
