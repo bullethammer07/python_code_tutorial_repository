02bff021cfaf71a5edba0cc8d26581aa9802e721
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-10-23 01:04:52 +0530

    sets tutorial added

---
+++ tutorial11_sets.py
@@ -0,0 +1,9 @@
+# creating set from a list
+
+list1 = [1, 2, 3, 4]
+set1 = set(list1)
+
+print("type", type(set1))
+print("set is : ", set1)
+
+# Follow methods in -> python_inbuilt_set_methods.py for all the set methods
