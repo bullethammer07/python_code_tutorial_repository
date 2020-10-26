341dabd781b6fd9ff739de772151590a770dd258
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-10-27 02:58:58 +0530

    added python import

---
+++ python_import.py
@@ -0,0 +1,17 @@
+import sys
+
+paths = sys.path
+print(paths) # This will return list of all path python searches files to import modules from
+
+# Importing contents from other files
+# for eg. we will port function 'my_function4' from the file 'python_args_kwargs.py' present in the projetc
+#         for that we can do as below
+
+from python_args_kwargs import my_function4
+
+my_function4("Stupid value")
+
+# To import a complete file you can use as below : (module is present in the project)
+
+import python_args_kwargs
+
