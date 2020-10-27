ea7390085adf36516ab3f92b5abe576ca67936b3
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-10-27 23:57:29 +0530

    update

--- python_generator.py
+++ python_generator.py
@@ -10,3 +10,11 @@
 
 # Python generators are a simple way of creating iterators. All the work we mentioned above are automatically handled by generators in Python.
 # Simply speaking, a generator is a function that returns an object (iterator) which we can iterate over (one value at a time).
+
+
+#------------------------------------------------------------------------------------------------------------
+# Example 1 : Create Generators in Python
+#             It is fairly simple to create a generator in Python.
+#             It is as easy as defining a normal function, but with a yield statement instead of a return statement.
+#             NOTE : The difference is that while a return statement terminates a function entirely,
+#             yield statement pauses the function saving all its states and later continues from there on successive calls.
