425eba2717d60159cc72a64c9b968bd4584de259
bullethammer07 <bullethammer07@users.noreply.github.com>
2020-11-10 09:59:46 +0530

    updated python sets

--- python_concepts_and_basics/python_sets.py
+++ python_concepts_and_basics/python_sets.py
@@ -6,4 +6,10 @@ set1 = set(list1)
 print("type", type(set1))
 print("set is : ", set1)
 
+#--------------------------------
+
+list2 = [1, 1, 2, 2, 3, 3, 3, 4, 4, 4]
+set2 = set(list2) # set 2 will conly contain 1,2,3 and 4
+print("set2 is : ", set2)
+
 # Follow methods in -> python_inbuilt_set_methods.py for all the set methods
